cec15600fdc4490e2597264f8d5984de49019ac1
AzuxDario <pawel_mibor95@tlen.pl>
2020-06-29 00:54:46 +0200

    Added LandpadParser

---
+++ Source/Parsers/Landpads/LandpadParser.cpp
@@ -0,0 +1,53 @@
+#include "LandpadParser.h"
+
+namespace Marsy::Parsers::LandpadParser
+{
+    LandpadParser::LandpadParser()
+    {
+
+    }
+
+    Landpad LandpadParser::parseLandpad(const std::string &input)
+    {
+        json j = json::parse(input);
+        return parseObject(j);
+    }
+
+    std::vector<Landpad> LandpadParser::parseLandpads(const std::string &input)
+    {
+        std::vector<Landpad> landpad;
+        json j = json::parse(input);
+
+        if(j.is_array())
+        {
+            for (json::iterator it = j.begin(); it != j.end(); ++it)
+            {
+                landpad.push_back(parseObject(it.value()));
+            }
+        }
+
+        return landpad;
+    }
+
+    Landpad LandpadParser::parseObject(const json &input)
+    {
+        Landpad landpad;
+        landpad.name = parseStringNullable(input, strName);
+        landpad.fullName = parseStringNullable(input, strFullName);
+        landpad.status = parseStringNullable(input, strStatus);
+        landpad.type = parseStringNullable(input, strType);
+        landpad.locality = parseStringNullable(input, strLocality);
+        landpad.region = parseStringNullable(input, strRegion);
+        landpad.latitude = parseFloatNullable(input, strLatitude);
+        landpad.longitude = parseFloatNullable(input, strLongitude);
+        landpad.landingAttempts = parseIntNullable(input, strLandingAttempts).value_or(landpad.landingAttempts.value());
+        landpad.landingSuccess = parseIntNullable(input, strLandingSuccesses).value_or(landpad.landingSuccess.value());
+        landpad.wikipedia = parseStringNullable(input, strWikipedia);
+        landpad.details = parseStringNullable(input, strDetails);
+        landpad.launches = parseArrayOfStringNullable(input, strLaunches);
+        landpad.id = parseStringNullable(input, strId);
+
+        return landpad;
+    }
+
+}
---
+++ Source/Parsers/Landpads/LandpadParser.h
@@ -0,0 +1,44 @@
+#ifndef MARSY_LANDPADS_LANDPADPARSER_H
+#define MARSY_LANDPADS_LANDPADPARSER_H
+
+#include <string>
+#include <vector>
+#include <optional>
+
+#include "../../Libraries/JSON/json.hpp"
+
+#include "../Parser.h"
+#include "../../Models/Landpads/Landpad.h"
+
+using json = nlohmann::json;
+using Marsy::Models::Landpads::Landpad;
+
+namespace Marsy::Parsers::LandpadParser
+{
+    class LandpadParser : public Parser
+    {
+    private:
+        const std::string strName = "name";
+        const std::string strFullName = "full_name";
+        const std::string strStatus = "status";
+        const std::string strType = "type";
+        const std::string strLocality = "locality";
+        const std::string strRegion = "region";
+        const std::string strLatitude = "latitude";
+        const std::string strLongitude = "longitude";
+        const std::string strLandingAttempts = "landing_attempts";
+        const std::string strLandingSuccesses = "landing_successes";
+        const std::string strWikipedia = "wikipedia";
+        const std::string strDetails = "details";
+        const std::string strLaunches = "launches";
+        const std::string strId = "id";
+    public:
+        LandpadParser();
+        Landpad parseLandpad(const std::string &input);
+        std::vector<Landpad> parseLandpads(const std::string &input);
+    private:
+        Landpad parseObject(const json &input);
+    };
+}
+
+#endif
