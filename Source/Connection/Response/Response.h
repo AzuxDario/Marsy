77cc4772e71361df5a2b0a04c94d353fbcc39dbd
AzuxDario <pawel_mibor95@tlen.pl>
2020-07-04 22:49:27 +0200

    Added response status

---
+++ Source/Connection/Response/Response.h
@@ -0,0 +1,10 @@
+#ifndef CONNECTION_RESPONSE_STATUS_H
+#define CONNECTION_RESPONSE_STATUS_H
+
+namespace Connection::Response
+{
+    enum class ResponseStatus {ok, badRequest, notFound, otherError}
+} 
+
+
+#endif
