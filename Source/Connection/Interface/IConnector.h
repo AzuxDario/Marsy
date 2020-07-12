543dd3d45d2e0a1acb1c4fbef1bfe97c0ff098cf
AzuxDario <pawel_mibor95@tlen.pl>
2020-07-12 23:04:04 +0200

    Added IConnector interface

---
+++ Source/Connection/Interface/IConnector.h
@@ -0,0 +1,19 @@
+#ifndef CONNECTION_ICONNECTOR_H
+#define CONNECTION_ICONNECTOR_H
+
+#include <string>
+#include <vector>
+#include <optional>
+
+#include "../Response/Response.h"
+
+namespace Connection
+{
+    class IConnector
+    {
+    public:
+        ApiResponse httpGet(std::string url);
+    };
+}
+
+#endif
--- Source/Connection/Response/Response.h
+++ Source/Connection/Response/Response.h
@@ -1,9 +1,18 @@
 #ifndef CONNECTION_RESPONSE_STATUS_H
 #define CONNECTION_RESPONSE_STATUS_H
 
-namespace Connection::Response
+#include <string>
+#include <optional>
+
+namespace Connection
 {
     enum class ResponseStatus {ok, badRequest, unauthorized, notFound, serverError, timeOut, otherError};
+
+    struct ApiResponse
+    {
+        ResponseStatus status;
+        std::optional<std::string> payload;
+    };
 } 
 
 
