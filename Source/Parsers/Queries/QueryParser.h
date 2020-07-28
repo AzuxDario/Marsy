18df98eaaf2f8232de64f89f8d90563091840786
AzuxDario <pawel_mibor95@tlen.pl>
2020-07-28 11:16:41 +0200

    Add file that wasn't sent

---
+++ Source/Parsers/Queries/QueryParser.h
@@ -0,0 +1,42 @@
+#ifndef MARSY_QUERIES_QUERYPARSER_H
+#define MARSY_QUERIES_QUERYPARSER_H
+
+#include <string>
+#include <vector>
+#include <optional>
+
+#include "../../Libraries/JSON/json.hpp"
+
+#include "../Parser.h"
+#include "../../Models/Queries/Request/QueryRequest.h"
+#include "../../Models/Queries/Response/QueryParameters.h"
+
+using json = nlohmann::json;
+using Marsy::Models::QueryModel::QueryRequest;
+using Marsy::Models::QueryModel::QueryParameters;
+
+namespace Marsy::Parsers::QueryParser
+{
+    class QueryParser : public Parser
+    {
+    private:
+        const std::string strTotalDocs = "totalDocs";
+        const std::string strOffset = "offset";
+        const std::string strLimit = "limit";
+        const std::string strTotalPage = "totalPages";
+        const std::string strPage = "page";
+        const std::string strPagingCounter = "pagingCounter";
+        const std::string strHasPrevPage = "hasPrevPage";
+        const std::string strHasNextPage = "hasNextPage";
+        const std::string strPrevPage = "prevPage";
+        const std::string strNextPage = "nextPage";
+    public:
+        QueryParser();
+        QueryParameters parseObject(const json &input);
+        QueryParameters parseObject(const std::string &input);
+    protected:
+        QueryParameters parseOne(const json &input);
+    };
+}
+
+#endif
