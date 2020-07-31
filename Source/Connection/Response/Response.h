04bb17ee758aef2ca7f1582ab93eef402b4b848e
AzuxDario <pawel_mibor95@tlen.pl>
2020-07-31 09:33:48 +0200

    Update Response.h

--- Source/Connection/Response/Response.h
+++ Source/Connection/Response/Response.h
@@ -6,7 +6,7 @@
 
 namespace Marsy::Connection
 {
-    enum class ResponseStatus {ok, badRequest, unauthorized, notFound, serverError, timeOut, otherError};
+    enum class ResponseStatus {ok = 200, badRequest = 400, unauthorized = 401, notFound = 404, serverError = 500, timeOut = 600, otherError = 700};
 
     struct ApiResponse
     {
